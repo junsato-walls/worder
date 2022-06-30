63f037c5de2ad2758cd8f9f79b93e5fdca4ba40f
junsato-walls <j.sato@walls-inc.com>
2022-06-30 16:10:26 +0900

    メニュースクロール機能追加

--- gui/worder/src/components/Menu/menu.jsx
+++ gui/worder/src/components/Menu/menu.jsx
@@ -7,9 +7,35 @@ import { useLocation, } from 'react-router-dom';
 //グリッドで分けている部分
 import Box from '@mui/material/Box';
 import Tab from '@mui/material/Tab';
-import TabContext from '@mui/lab/TabContext';
-import TabList from '@mui/lab/TabList';
-import TabPanel from '@mui/lab/TabPanel';
+import Tabs from '@mui/material/Tabs';
+import Typography from '@mui/material/Typography';
+
+function TabPanel(props) {
+  const { children, value, index, ...other } = props;
+
+  return (
+    <div
+      role="tabpanel"
+      hidden={value !== index}
+      id={`simple-tabpanel-${index}`}
+      aria-labelledby={`simple-tab-${index}`}
+      {...other}
+    >
+      {value === index && (
+        <Box sx={{ p: 3 }}>
+          <Typography>{children}</Typography>
+        </Box>
+      )}
+    </div>
+  );
+}
+
+function a11yProps(index) {
+  return {
+    id: `simple-tab-${index}`,
+    'aria-controls': `simple-tabpanel-${index}`,
+  };
+}
 
 function Menu() {
   const baseURL = "http://127.0.0.1:8000";
@@ -28,27 +54,33 @@ function Menu() {
   }
 
   const handleChange = (event, newValue) => {
+    console.log(newValue)
     setValue(newValue);
   };
 
   return (
     <>
-      <Box sx={{ width: '100%', bgcolor: 'background.paper' }}>
-        <TabContext value={value}>
-          <Box sx={{ borderBottom: 1, borderColor: 'divider' }}>
-            <TabList onChange={handleChange}>
+      <Box sx={{ width: '100%' }}>
+        <Box sx={{ borderBottom: 1, borderColor: 'divider' }}>
+          <Tabs
+            value={value}
+            onChange={handleChange}
+            variant="scrollable"
+            scrollButtons
+            allowScrollButtonsMobile
+            aria-label="scrollable force tabs example"
+          >
               {categoryData.map((category, i) =>
-                <Tab label={category.category} value={i} />
+                <Tab label={category.category} {...a11yProps(i)} />
               )}
-            </TabList>
-          </Box>
-          {categoryData.map((category, i) =>
-            <TabPanel value={i}><MenuTab 
-              category_id={category.id} 
-               />
-            </TabPanel>
-          )}
-        </TabContext>
+
+          </Tabs>
+        </Box>
+        {categoryData.map((category, i) =>
+          <TabPanel value={value} index={i}>
+            <MenuTab category_id={category.id}/>
+          </TabPanel>
+        )}
       </Box>
     </>
   )
